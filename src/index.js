d565e9fb6fba951d394124733427bb34acb68eb6
Mikkel Fishman <mikkel@pearl.link>
2021-03-05 17:14:12 +1300

    Fixing EOL which isn't working on windows

--- src/index.js
+++ src/index.js
@@ -98,7 +98,7 @@ function commentLine (line) {
 
 module.exports = function (source) {
   try {
-    const sourceByLine = source.split(os.EOL)
+    const sourceByLine = source.split('\n')
     const blocks = searchBlocks(sourceByLine)
     const truthyBlocks = getTruthyBlocks(blocks)
     const transformedSource = commentCodeInsideBlocks(sourceByLine, truthyBlocks)
